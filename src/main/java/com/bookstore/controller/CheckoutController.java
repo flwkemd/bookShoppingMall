bbed5b6b48b4b763b0af0997ab7cdd673ee4d49e
flwkemd <flwkemd@naver.com>
2018-01-14 00:38:27 +0100

    implemented order service & controller

--- src/main/java/com/bookstore/controller/CheckoutController.java
+++ src/main/java/com/bookstore/controller/CheckoutController.java
@@ -1,17 +1,22 @@
 package com.bookstore.controller;
 
 import java.security.Principal;
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bookstore.domain.BillingAddress;
 import com.bookstore.domain.CartItem;
+import com.bookstore.domain.Order;
 import com.bookstore.domain.Payment;
 import com.bookstore.domain.ShippingAddress;
 import com.bookstore.domain.ShoppingCart;
@@ -21,8 +26,10 @@ import com.bookstore.domain.UserPayment;
 import com.bookstore.domain.UserShipping;
 import com.bookstore.service.BillingAddressService;
 import com.bookstore.service.CartItemService;
+import com.bookstore.service.OrderService;
 import com.bookstore.service.PaymentService;
 import com.bookstore.service.ShippingAddressService;
+import com.bookstore.service.ShoppingCartService;
 import com.bookstore.service.UserPaymentService;
 import com.bookstore.service.UserService;
 import com.bookstore.service.UserShippingService;
@@ -55,6 +62,15 @@ public class CheckoutController {
 	
 	@Autowired
 	private UserPaymentService userPaymentService;
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	@Autowired
+	private ShoppingCartService shoppingCartService;
+	
+	@Autowired
+	private OrderService orderService;
 	
 	@RequestMapping("/checkout")
 	public String checkout(
@@ -134,7 +150,69 @@ public class CheckoutController {
 		return "checkout";
 	}
 	
-	@RequestMapping("/setShippingAddress")
+	@RequestMapping(value="/checkout", method=RequestMethod.POST){
+		public String checkoutPost(
+				@ModelAttribute("shippingAddress") ShippingAddress shippingAddress,
+				@ModelAttribute("billinggAddress") BillingAddress billingAddress,
+				@ModelAttribute("payment") Payment payment,
+				@ModelAttribute("billingSameAsShipping") String billingSameAsShipping,
+				@ModelAttribute("shippingMethod") String shippingMethod,
+				Principal principal, Model model) {
+			
+			ShoppingCart shoppingCart = userService.findByUsername(principal.getName()).getShoppingCart();
+			
+			List<CartItem> cartItemList = cartItemService.findByShoppingCart(shoppingCart);
+			model.addAttribute("cartItemList", cartItemList);
+			
+			if(billingSameAsShipping.equals("true")) {
+				billingAddress.setBillingAddressName(shippingAddress.getShippingAddressName());
+				billingAddress.setBillingAddressStreet1(shippingAddress.getShippingAddressStreet1());
+				billingAddress.setBillingAddressStreet2(shippingAddress.getShippingAddressStreet2());
+				billingAddress.setBillingAddressCity(shippingAddress.getShippingAddressCity());
+				billingAddress.setBillingAddressState(shippingAddress.getShippingAddressState());
+				billingAddress.setBillingAddressCountry(shippingAddress.getShippingAddressCountry());
+				billingAddress.setBillingAddressZipcode(shippingAddress.getShippingAddressZipcode());
+			}
+			
+			if(shippingAddress.getShippingAddressStreet1().isEmpty() || 
+				shippingAddress.getShippingAddressCity().isEmpty() ||
+				shippingAddress.getShippingAddressState().isEmpty() ||
+				shippingAddress.getShippingAddressName().isEmpty() ||
+				shippingAddress.getShippingAddressZipcode().isEmpty() ||
+				payment.getCardNumber().isEmpty() ||
+				payment.getCvc() == 0 ||
+				billingAddress.getBillingAddressStreet1().isEmpty() ||
+				billingAddress.getBillingAddressCity().isEmpty() ||
+				billingAddress.getBillingAddressState().isEmpty() ||
+				billingAddress.getBillingAddressName().isEmpty() ||
+				billingAddress.getBillingAddressZipcode().isEmpty()
+				)
+				
+				return "redirect:/checkout?id="+shoppingCart.getId()+"&missingRequiredField=true";
+		
+			User user = userService.findByUsername(principal.getName());
+			
+			Order order = orderService.createOrder(shoppingCart, shippingAddress, billingAddress, payment, shippingMethod, user);
+
+			mailSender.send(mailConstructor.constructOrderConfirmationEmail(user, order, Locale.KOREAN));
+		
+			shoppingCartService.clearShoppingCart(shoppingCart);
+			
+			LocalDate today = LocalDate.now();
+			LocalDate estimatedDeliveryDate;
+			
+			if(shippingMethod.equals("groundShipping")) {
+				estimatedDeliveryDate = today.plusDays(5);
+			}else {
+				estimatedDeliveryDate = today.plusDays(2);
+			}
+			
+			model.addAttribute("estimatedDeliveryDate", estimatedDeliveryDate);
+			
+			return "orderSubmittedPage";
+		}
+
+		@RequestMapping("/setShippingAddress")
 	public String setShippingAddress(
 			@RequestParam("userShippingId") Long userShippingId, Model model, Principal principal) {
 		
--- src/main/java/com/bookstore/domain/ShippingAddress.java
+++ src/main/java/com/bookstore/domain/ShippingAddress.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class ShippingAddress {
@@ -21,9 +22,8 @@ public class ShippingAddress {
 	private String ShippingAddressCountry;
 	private String ShippingAddressZipcode;
 	
-	@ManyToOne
-	@JoinColumn(name = "user_id")
-	private User user;
+	@OneToOne
+	private Order order;
 
 	public Long getId() {
 		return id;
@@ -89,11 +89,12 @@ public class ShippingAddress {
 		ShippingAddressZipcode = shippingAddressZipcode;
 	}
 
-	public User getUser() {
-		return user;
+	public Order getOrder() {
+		return order;
 	}
 
-	public void setUser(User user) {
-		this.user = user;
+	public void setOrder(Order order) {
+		this.order = order;
 	}
+
 }
